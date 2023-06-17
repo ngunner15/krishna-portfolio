f80aa7ed01017306ddb3256437e41782062f1b6b
Nisarg Vadgama <nisargvv@gmail.com>
2023-06-17 15:08:46 -0700

    Add hamburger menu for mobile screens

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -37,28 +37,63 @@ const Navbar = () => {
       <div className="justify-between md:items-center md:flex">
         <div>
           <div className="md:py-5 md:block">
-            <h2 className="text-2xl font-bold">Krishna Vadgama</h2>
+            <Link to="home">
+              <div className="container flex items-center space-x-2">
+                <h2 className="text-2xl font-bold">Krishna Vadgama</h2>
+              </div>
+            </Link>
+            <div className="md:hidden">
+              <button
+                className="p-2 text-gray-700 dark:text-white rounded-md outline-none focus:border-gray-400 focus:border"
+                onClick={() => setNavbar(!navbar)}
+              >
+                {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
+              </button>
+            </div>
           </div>
         </div>
-        <div className="md:flex md:space-x-6 justify-center items-center">
-          {NAV_ITEMS.map((item, idx) => {
-            return <a key={idx}>{item.label}</a>
-          })}
-          {currentTheme === "dark" ? (
-            <button
-              onClick={() => setTheme("light")}
-              className="bg-slate-100 p-2 rounded-xl"
-            >
-              <RiSunLine size={25} color="black" />
-            </button>
-          ) : (
-            <button
-              onClick={() => setTheme("dark")}
-              className="bg-slate-100 p-2 rounded-xl"
-            >
-              <RiMoonFill size={25} />
-            </button>
-          )}
+
+        <div
+          className={`justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+            navbar ? "block" : "hidden"
+          }`}
+        >
+          <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+            {NAV_ITEMS.map((item, idx) => {
+              return (
+                <Link
+                  key={idx}
+                  to={item.page}
+                  className={
+                    "block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100"
+                  }
+                  activeClass="active"
+                  spy={true}
+                  smooth={true}
+                  offset={-100}
+                  duration={500}
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  {item.label}
+                </Link>
+              )
+            })}
+            {currentTheme === "dark" ? (
+              <button
+                onClick={() => setTheme("light")}
+                className="bg-slate-100 p-2 rounded-xl"
+              >
+                <RiSunLine size={25} color="black" />
+              </button>
+            ) : (
+              <button
+                onClick={() => setTheme("dark")}
+                className="bg-slate-100 p-2 rounded-xl"
+              >
+                <RiMoonFill size={25} />
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </header>
