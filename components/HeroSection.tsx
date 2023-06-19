628cc8791870c00b8062321af5c9bc1af231bff1
Nisarg Vadgama <nisargvv@gmail.com>
2023-06-18 18:51:03 -0700

    Create home page

---
+++ components/HeroSection.tsx
@@ -0,0 +1,34 @@
+"use client"
+import React from 'react'
+import Image from 'next/image'
+
+const HeroSection = () => {
+  return (
+    <section id="home">
+      <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
+        <div className="md:mt-2 md:w-1/2">
+          <Image
+            src="/headshot_01.jpg"
+            alt="headshot"
+            width={325}
+            height={325}
+            className="rounded-full shadow-2xl"
+          />
+        </div>
+        <div className="md:mt-2 md:w-3/5">
+          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Krishna!</h1>
+          <p className="text-lg mt-4 mb-6 md:text-2xl">
+            I&#39;m a{" "}
+            <span className="font-semibold text-teal-600">
+              Physiotherapist{" "}
+            </span>
+            based in Savarkundla, Gujarat, India. Working towards treating and taking care of patients that
+            makes their lives easier.
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default HeroSection
