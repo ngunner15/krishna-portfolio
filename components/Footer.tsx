c4d173e347045be5cb2d144a7a55fffc88b379ce
Nisarg Vadgama <nisargvv@gmail.com>
2023-06-25 18:02:48 -0700

    Add margin

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ import {
 
 const Footer = () => {
   return (
-    <footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl border-t-2">
+    <footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl border-t-2 mt-8">
       <hr className="w-full h-0.5 mx-auto mt-8 border-0"></hr>
       <div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
         <div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">
