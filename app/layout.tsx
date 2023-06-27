4caa729f2fb0986df16e8282bca8c836dcd89b67
Nisarg Vadgama <nisargvv@gmail.com>
2023-06-26 22:46:04 -0700

    Remove next/font and add custom links

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,21 +3,6 @@ import '../styles/globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import { ThemeProvider } from 'next-themes'
-import { Philosopher, Happy_Monkey } from '@next/font/google'
-
-const philosopher = Philosopher({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-philosopher',
-})
-
-const happyMonkey = Happy_Monkey({
-  weight: '400',
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-monkey',
-})
 
 export default function RootLayout({
   children,
@@ -30,8 +15,11 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Happy+Monkey&family=Philosopher:wght@400;700&display=swap" rel="stylesheet"></link>
       <head />
-      <body className={`${philosopher.variable} ${happyMonkey.variable} scrollbar scrollbar-track-wBlack scrollbar-thumb-wRed hover:scrollbar-thumb-wHoverRed dark:scrollbar-track-dEmerald dark:scrollbar-thumb-dBlack dark:hover:scrollbar-thumb-dHoverBlack scrollbar-thumb-rounded-lg bg-wYellow text-wBlack border-wBlack dark:text-dGreen dark:bg-dBlack`}>
+      <body className="scrollbar scrollbar-track-wBlack scrollbar-thumb-wRed hover:scrollbar-thumb-wHoverRed dark:scrollbar-track-dEmerald dark:scrollbar-thumb-dBlack dark:hover:scrollbar-thumb-dHoverBlack scrollbar-thumb-rounded-lg bg-wYellow text-wBlack border-wBlack dark:text-dGreen dark:bg-dBlack">
         <div className="top-0 right-0 left-0 bottom-0 fixed border-solid border-4 dark:border-dEmerald"></div>
         <ThemeProvider enableSystem={true} attribute="class">
           <Navbar />
