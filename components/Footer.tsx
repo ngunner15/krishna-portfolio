edad03bf4957f155dbeb6ef13d7c2c7a15b48097
Nisarg Vadgama <nisargvv@gmail.com>
2023-06-27 08:52:52 -0700

    Add bottom border and make footer full width in md

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,9 +6,9 @@ import {
 
 const Footer = () => {
   return (
-    <footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl border-t-2 mt-8">
-      <hr className="w-full h-0.5 mx-auto mt-8 border-0"></hr>
-      <div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
+    <footer className="mx-auto max-w-full px-4 sm:px-6 border-t-2 mt-8 md:mt-12">
+      <hr className="w-full h-0.5 mx-auto mt-1 border-0"></hr>
+      <div className="mx-auto p-4 md:px-16 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
         <div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">
           © 2023 Nisarg Vadgama<a href="/" className="hover:underline"></a>
         </div>
@@ -31,6 +31,7 @@ const Footer = () => {
           </a>
         </div>
       </div>
+      <hr className="fixed bottom-0 left-0 w-full mx-auto mt-1 border-4 dark:border-dEmerald"></hr>
     </footer>
   )
 }
