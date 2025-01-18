53c175db0e010553ff00b599ea0d016e4aa3ae3a
Nisarg Vadgama <nisargvv@gmail.com>
2025-01-17 20:58:02 -0800

    feat: add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import HeroSection from "@/components/HeroSection"
 import AboutSection from "@/components/AboutSection"
 import ImageSection from "@/components/ImageSection"
+import { Metadata } from "next"
 
+export const metadata: Metadata = {
+  title: 'Krishna Vadgama 👋',
+  description: 'Welcome to the homepage of Krishna Vadgama\'s website',
+}
 export default function Home() {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
