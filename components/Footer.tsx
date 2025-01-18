551e85725c0973006b8a92f787ca806429a41757
Nisarg Vadgama <nisargvv@gmail.com>
2025-01-17 20:12:38 -0800

    fix: add z-index

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,10 +10,10 @@ const Footer = () => {
     <footer className="mx-auto max-w-full px-4 sm:px-6 border-t-2 mt-8 md:mt-12">
       <hr className="w-full h-0.5 mx-auto mt-1 border-0"></hr>
       <div className="mx-auto p-4 md:px-16 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
-        <div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">
+        <div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100 ">
           © {currentYear} Nisarg Vadgama<a href="/" className="hover:underline"></a>
         </div>
-        <div className="flex flex-row items-center justify-center space-x-2 mb-1">
+        <div className="flex flex-row items-center justify-center space-x-2 mb-1 z-10">
           <a href="https://github.com/ngunner15" rel="noreferrer" target="_blank">
             <AiOutlineGithub
               className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
